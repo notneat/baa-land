06bbf4c1d727194092cf2c07b5071a184d19bdae
Lanzlol <gabriellanz.glv@gmail.com>
2023-08-19 12:10:26 -0300

    ai da ovelha tá a caminho

---
+++ Assets/SheepController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SheepController : MonoBehaviour
+{ 
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
